339a5009d065b93a431f3015097068b9e054c1e5
Anthony <125139205@qq.com>
2021-01-12 10:21:59 +0800

    qos directory and routing manager service skeleton

---
+++ src/main/java/net/floodlightcontroller/qos/IQoSRoutingManagementService.java
@@ -0,0 +1,8 @@
+package net.floodlightcontroller.qos;
+
+import net.floodlightcontroller.core.module.IFloodlightService;
+
+public interface IQoSRoutingManagementService extends IFloodlightService {
+    // TODO check the QoS parameter of each link within the Map
+    // TODO call back QoS routing calculation to get tne reasonable routing stragegy
+}
