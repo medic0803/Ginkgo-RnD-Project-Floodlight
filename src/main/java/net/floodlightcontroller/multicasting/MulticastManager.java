24fe007ef19acb12d6fcb8bbe1b1e0ae74f077e9
Anthony <125139205@qq.com>
2021-02-27 15:11:25 +0800

    determine statement for the igmp join/leave meesage

--- src/main/java/net/floodlightcontroller/multicasting/MulticastManager.java
+++ src/main/java/net/floodlightcontroller/multicasting/MulticastManager.java
@@ -32,6 +32,19 @@ public class MulticastManager implements IOFMessageListener, IFloodlightModule {
         if (eth.getEtherType() == EthType.IPv4){
             if (((IPv4)eth.getPayload()).getProtocol() == IpProtocol.IGMP){
                 System.out.println("-----------receive igmp packet ----------------");
+                System.out.println("Source Address is: " + ((IPv4)eth.getPayload()).getSourceAddress());
+                System.out.println("Destination Address is: " + ((IPv4)eth.getPayload()).getDestinationAddress());
+
+                // TODO: determine IGMP join or leave message
+                byte[] igmpPayload = eth.getPayload().serialize();
+                System.out.println("Payload length = " + igmpPayload.length);
+                // the total lengeth of this packet is 54, the previous 14(0-13) is for header, the rest 40 is for paylod, and the 46/32 is for record type
+                if (igmpPayload[32] == 4){
+                    System.out.println(igmpPayload + "IGMP join message");
+                } else if (igmpPayload[32] == 3){
+                    System.out.println(igmpPayload + "IGMP leave message");
+                }
+
             }
         }
         return Command.CONTINUE;
