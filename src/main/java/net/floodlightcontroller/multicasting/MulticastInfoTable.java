523adc32460ff7d0b3cdfbbd856d3684066447d1
Anthony <125139205@qq.com>
2021-03-01 14:47:16 +0800

    determine statement for composing multicast group information table

---
+++ src/main/java/net/floodlightcontroller/multicasting/MulticastInfoTable.java
@@ -0,0 +1,11 @@
+package net.floodlightcontroller.multicasting;
+
+import net.floodlightcontroller.packet.IPacket;
+import org.projectfloodlight.openflow.types.IPAddress;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class MulticastInfoTable extends HashMap<IPAddress, Set<IPAddress>>{
+
+}
--- src/main/java/net/floodlightcontroller/multicasting/MulticastManager.java
+++ src/main/java/net/floodlightcontroller/multicasting/MulticastManager.java
@@ -13,16 +13,17 @@ import net.floodlightcontroller.packet.IPv4;
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.OFType;
 import org.projectfloodlight.openflow.types.EthType;
+import org.projectfloodlight.openflow.types.IPAddress;
 import org.projectfloodlight.openflow.types.IpProtocol;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 
 
 public class MulticastManager implements IOFMessageListener, IFloodlightModule {
 
+    // Instance field
     protected IFloodlightProviderService floodlightProvider;
+    private MulticastInfoTable multicastInfoTable = new MulticastInfoTable();
 
     @Override
     public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
@@ -31,25 +32,55 @@ public class MulticastManager implements IOFMessageListener, IFloodlightModule {
                         IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
         if (eth.getEtherType() == EthType.IPv4){
             if (((IPv4)eth.getPayload()).getProtocol() == IpProtocol.IGMP){
+
+                // TODO: Delete
                 System.out.println("-----------receive igmp packet ----------------");
                 System.out.println("Source Address is: " + ((IPv4)eth.getPayload()).getSourceAddress());
                 System.out.println("Destination Address is: " + ((IPv4)eth.getPayload()).getDestinationAddress());
 
-                // TODO: determine IGMP join or leave message
+                IPAddress multicastGroupIPAddress = ((IPv4)eth.getPayload()).getDestinationAddress();
+                IPAddress hostIPAddress = ((IPv4)eth.getPayload()).getSourceAddress();
+
                 byte[] igmpPayload = eth.getPayload().serialize();
                 System.out.println("Payload length = " + igmpPayload.length);
                 // the total lengeth of this packet is 54, the previous 14(0-13) is for header, the rest 40 is for paylod, and the 46/32 is for record type
                 if (igmpPayload[32] == 4){
                     System.out.println(igmpPayload + "IGMP join message");
+                    if (multicastInfoTable.isEmpty()){  // empty multicast information table
+                        HashSet<IPAddress> newMulticastGroup = new HashSet();
+                        newMulticastGroup.add(hostIPAddress);
+                        multicastInfoTable.put(multicastGroupIPAddress, newMulticastGroup);
+                    } else{ // non-empty table
+                        if (multicastInfoTable.containsValue(multicastGroupIPAddress)){
+                            if (multicastInfoTable.get(multicastGroupIPAddress).contains(hostIPAddress)){   // host already join the multicast group
+                                // nothing happen
+                            } else {    // host has not joined the multicast group yes
+                               multicastInfoTable.get(multicastGroupIPAddress).add(hostIPAddress);
+                                // TODO: use algorithm to analyse
+                                // getMulticastRoutingDecision(multicastInfoTable.keySet(multicastGroupIPAddress));
+                            }
+                        } else {    // multicast group IP address do not exist
+                            HashSet<IPAddress> newMulticastGroup = new HashSet();
+                            newMulticastGroup.add(hostIPAddress);
+                            multicastInfoTable.put(multicastGroupIPAddress, newMulticastGroup);
+                            // TODO: use algorithm to analyse
+                            // getMulticastRoutingDecision(multicastInfoTable.keySet(multicastGroupIPAddress));
+                        }
+                    }
                 } else if (igmpPayload[32] == 3){
                     System.out.println(igmpPayload + "IGMP leave message");
                 }
+
 
             }
         }
         return Command.CONTINUE;
     }
 
+
+    private void pushMulticastGroupRoute(){
+        // TODO::
+    }
     @Override
     public String getName() {
         return "multicasting";
